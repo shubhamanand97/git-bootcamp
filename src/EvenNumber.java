932c12615bee22352429bb49661f0af069981b13
Shubham Anand <shubhamanand97@gmail.com>
2018-12-08 00:19:37 +0530

    Initial Project imported

---
+++ src/EvenNumber.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Shubham Anand on 12/7/2018.
+ **/
+
+public class EvenNumber
+{
+    public static void main(String[] args)
+    {
+        int a=100;
+        for(int i=0;i<=a;i+=2)
+        {
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Shubham Anand on 12/7/2018.
+ **/
+
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+}
